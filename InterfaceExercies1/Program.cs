8e36d40a8a5373dc9cd3fa67d3ba0ae7836f9a0b
herkommer <m@herkommer.co.uk>
2021-01-14 11:59:49 +0100

    initial commit

---
+++ InterfaceExercies1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercies1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            c.Accelerate();
+            c.Indicate("left");
+            c.Accelerate();
+            c.Brake();
+            c.Indicate("right");
+           
+
+            //
+            ICar myCar = new Car();
+            ICarService myCarService = new Car();
+
+            
+            
+
+        }
+    }
+
+    //Abstract, no code only really good naming
+    interface ICar
+    {
+        void Brake();
+        void Accelerate();
+        void Indicate(string parameter);
+
+    }
+    interface ICarService
+    {
+        int OilPressure();
+    }
+
+    //Concrete, implements Interface
+    class Car : ICar, ICarService
+    {
+        public int OilPressure()
+        {
+            return 42;
+        }
+        public void Brake()
+        {
+            Console.WriteLine("Car is braking");
+        }
+
+        public void Accelerate()
+        {
+            Console.WriteLine("Car is accelerating");
+
+        }
+
+        public void Indicate(string parameter)
+        {
+            if (parameter == "left")
+            {
+                Console.WriteLine("Indicating left turn");
+            }
+            if (parameter == "right")
+            {
+                Console.WriteLine("Indicating right turn");
+            }
+        }
+    }
+}
---
+++ InterfaceExercise2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            IRepository repo = new MongoDBCarDataStorage();
+            repo.AddNewCar(c);
+
+        }
+    }
+
+    interface IRepository
+    {
+        int GetNumberOfCars();
+        void AddNewCar(Car c);
+
+        void RemoveCar(Car c);
+    }
+
+    //Concrete
+    class JsonCarData : IRepository
+    {
+        public void AddNewCar(Car c)
+        {
+            Console.WriteLine("Add new car in JSON");
+        }
+
+        public int GetNumberOfCars()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCar(Car c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class MongoDBCarDataStorage : IRepository
+    {
+        public void AddNewCar(Car c)
+        {
+            Console.WriteLine("Adding car to MongoDB Atlas");
+        }
+
+        public int GetNumberOfCars()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveCar(Car c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Car
+    {
+
+    }
+}
